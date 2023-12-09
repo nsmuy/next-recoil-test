8f18e03ce316ddfd9a69c3d73d2bc77cd58c26ba
yuumi <gin728emma218@gmail.com>
2023-12-09 17:00:32 +0900

    first commit

---
+++ src/app/_app.js
@@ -0,0 +1,12 @@
+import './globals.css'
+import { RecoilRoot } from "recoil"; //追加
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
+}
+
+export default MyApp;
